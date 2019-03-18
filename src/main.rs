e0d3680c808f068b4ec8ac2e6078b1237534303e
Ralph Giles <giles@thaumas.net>
2019-03-17 22:28:45 -0700

    Sketch directory traversal.
    Just feeling out how the DirEntry api works.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::io;
+
+fn walk(path: &str) -> io::Result<()> {
+    for entry in std::fs::read_dir(path)? {
+        let file = entry?;
+        println!("{:?}", file.path());
+    }
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, world! Let's see what we have...");
+    walk(".").expect("Failed to walk directory");
 }
