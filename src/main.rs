b22db545ec4e6adf10374cd0328b717968c9e716
Ralph Giles <giles@thaumas.net>
2019-05-15 23:26:09 -0700

    Ignore dotfiles.
    If the file or directory name starts with a dot, skip it.
    
    Perhaps not what we eventually want for a scrubber, but
    makes testing simpler at this stage.
    
    The `if let` clause is a bit clunky, but have to deal with
    multiple possible conversion errors. Maybe .to_string_lossy()
    would be more appropriate for this sort of task?

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,12 @@ use std::io;
 fn walk(start_path: &str) -> io::Result<()> {
     for entry in std::fs::read_dir(start_path)? {
         let path = entry?.path();
+        if let Some(name) = path.file_name().and_then(|x| x.to_str()) {
+            // Skip dotfiles and directories.
+            if name.starts_with(".") {
+                continue;
+            }
+        }
         if path.is_dir() {
             println!("directory {:?}", path);
         } else {
